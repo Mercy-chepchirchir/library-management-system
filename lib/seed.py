a7599d4a9462258735afa3ccd3699d9c5c99eb38
mercy-chepchirchir <mercychepchirchir33@gmail.com>
2023-09-08 00:02:15 +0300

    creat a student object and append to students list

--- lib/seed.py
+++ lib/seed.py
@@ -59,4 +59,22 @@ def initialize_db():
         
         journal_id_number = journal_id_number + 1
         
+    students = []
+    
+    student_id_number = 1
+    for i in range(100):
+        student_id_value = f"S0{student_id_number}"#generates a unique student ID using the student_id_number variable
+        
+        student = Student(
+            student_id=student_id_value,
+            name=fake.name() # generate a fake student name.
+            
+        )
+        
+        session.add(student) #adds the newly created Student object to the SQLAlchemy session
+        session.commit() #inserts the student record into the database.
+        
+        students.append(student_id_value) #The generated student_id_value  is added to the students list
+        
+        student_id_number = student_id_number + 1
     
