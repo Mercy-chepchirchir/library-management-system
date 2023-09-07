c3e8d8243ed87b1255fc11eb254d2a062d53a805
mercy-chepchirchir <mercychepchirchir33@gmail.com>
2023-09-07 21:26:49 +0300

    run migrations ro add book model

--- lib/models.py
+++ lib/models.py
@@ -3,13 +3,28 @@ from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Float, cre
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+
 # Create a SQLAlchemy database engine
 engine = create_engine('sqlite:///library.db')
 
 # Create a base class for declarative models
 Base = declarative_base()
 
+
 # used to interact with the database
 Session = sessionmaker(bind=engine)
 
+# Define the Book class with attributes and relationships
+class Book(Base):
+    __tablename__ = 'books'
 
+    id = Column(Integer, primary_key=True)
+    book_id = Column(String, unique=True, nullable=False)
+    title = Column(String, nullable=False)
+    author = Column(String, nullable=False)
+    total_copies = Column(Integer, nullable=False, default=1)
+    available_copies = Column(Integer, nullable=False, default=1)
+    fee_per_day = Column(Float, default=0.00)  # Adjust fee as needed
+
+    book_transactions = relationship('BookTransaction', back_populates='book')
+    
