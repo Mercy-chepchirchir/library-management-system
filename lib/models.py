d32321d17972d329da5ffabf6d01c456e8d4e438
mercy-chepchirchir <mercychepchirchir33@gmail.com>
2023-09-07 20:58:46 +0300

    Created a SQLAlchemy database engine

--- lib/models.py
+++ lib/models.py
@@ -3,4 +3,5 @@ from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Float, cre
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-
+# Create a SQLAlchemy database engine
+engine = create_engine('sqlite:///library.db')
